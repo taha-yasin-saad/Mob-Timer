9ae9106eed54d80a5dcea7c5ff6cc1cafb9c6724
taha-yasin-saad <87243489+taha-yasin-saad@users.noreply.github.com>
2021-11-13 18:45:13 +0200

    initial commit

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+
+// Import ng-circle-progress
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    // Specify ng-circle-progress as an import
+    NgCircleProgressModule.forRoot({
+      // set defaults here
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300,
+      animation: false,
+      responsive: true,
+      renderOnClick:false
+    })
+  ],
+  declarations: [HomePage]
+})
+export class HomePageModule {}
